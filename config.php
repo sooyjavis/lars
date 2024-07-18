c23c5210fbd485282502127da609c18cbdb74d98
sooyjavis <licaifa07@gmail.com>
2024-07-18 13:48:14 -0600

    Merge branch 'main' of https://github.com/Senk-Git/lars

--- config.php
+++ config.php
@@ -1,13 +1,8 @@
 <?php
-
-// Creando una nueva conexión a la base de datos.
 $conn = new mysqli("localhost", "root", "", "lars");
 
-// Comprobando si hay un error de conexión.
 if ($conn->connect_error) {
     echo 'Error de conexion ' . $conn->connect_error;
     exit;
 }
-
-
 ?>
